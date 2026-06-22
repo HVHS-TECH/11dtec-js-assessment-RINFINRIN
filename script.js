81d5712b6634b7c673875d13d79980e3c316276a
RINFINRIN <24154rs@hvhs.school.nz>
2026-06-22 21:51:39 +0000

    ok

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+/***Variables***/
+var CustomerName;
+var Doughnuts
+
 console.log("Hello world!")
+alert("Hello, Welcome to Dingle Doughnuts")
+CustomerName=prompt("What is your name?")
+console.log(CustomerName)
+alert("Hello " + CustomerName + " we have the best doughnuts")
+alert let Doughnuts = ["Salt and Pepper", "Healthy Grass", "Thai Chilli"]
