d4039dd460bffa5f5f7ea8128ce84108c3ec28f4
RINFINRIN <24154rs@hvhs.school.nz>
2026-06-25 00:47:11 +0000

    Jun 25, 2026, 12:47 AM

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /***Variables***/
 var CustomerName;
 var Doughnuts
+var Doughnuts = ["Salt and Pepper", "Healthy Grass", "Thai Chilli"]
 
 console.log("Hello world!")
 alert("Hello, Welcome to Dingle Doughnuts")
 CustomerName=prompt("What is your name?")
 console.log(CustomerName)
 alert("Hello " + CustomerName + " we have the best doughnuts")
-alert let Doughnuts = ["Salt and Pepper", "Healthy Grass", "Thai Chilli"]
