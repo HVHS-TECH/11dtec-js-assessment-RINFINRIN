6d66aec24771f5e93556fa2c000488a8d0f8b484
RINFINRIN <24154rs@hvhs.school.nz>
2026-06-25 01:03:42 +0000

    Jun 25, 2026, 1:03 AM

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var CustomerName;
 var Doughnuts
 var Doughnuts = ["Salt and Pepper", " Healthy Grass", " Thai Chilli"]
-var Money = 50
+var CustomerMoney = 50
 
 console.log("Hello world!")
 alert("Hello, Welcome to Dingle Doughnuts")
@@ -11,5 +11,5 @@ console.log(CustomerName)
 alert("Hello " + CustomerName + " we have the best doughnuts")
 console.log(Doughnuts)
 alert("These are the doughnuts we have to offer. " + Doughnuts)
-alert("You have " + Money + "$ to spend o out epic doughnuts")
+alert("You have " + CustomerMoney + "$ to spend o out epic doughnuts")
 alert("each Doughnut will cost you 10$ bucks")
