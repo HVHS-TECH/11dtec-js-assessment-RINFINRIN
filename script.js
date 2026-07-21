fe65c7f1d403c0fbf9e0ad36620ebf71fa88a216
RINFINRIN <24154rs@hvhs.school.nz>
2026-07-21 23:52:40 +0000

    Jul 21, 2026, 11:52 PM

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var CustomerName;
 var Doughnuts
 var Doughnuts = ["Salt and Pepper", " Healthy Grass", " Thai Chilli", " Kung Pao", " Pickles & Onion"]
 var CustomerMoney = 50
-
+var userMoney
 console.log("Hello world!")
 alert("Hello, Welcome to Dingle Doughnuts")
 CustomerName=prompt("What is your name?")
@@ -14,4 +14,6 @@ alert("These are the doughnuts we have to offer. " + Doughnuts)
 alert("You have " + CustomerMoney + "$ to spend on our epic doughnuts")
 alert("Each Doughnut will cost you 10$ bucks")
 Doughnuts=prompt("What doughnut would you like?")
-alert("The " + Doughnuts + " will cost you 10 bucks")
+alert("The " + Doughnuts + " doughnut will cost you 10 bucks")
+CustomerMoney - 10
+alert("You have got " + CustomerMoney + " left to spend")
